5daa54cd74ddca9ba9741658ed2f76f07ad6b661
Schmitz Marlon <marlonschmitz289@gmail.com>
2025-04-08 14:55:14 +0200

    Fixed UserEditor and added StartupLocation to UserSelection

--- BoardEditorJeopardy/UserEditorWindow.xaml.cs
+++ BoardEditorJeopardy/UserEditorWindow.xaml.cs
@@ -1,21 +1,49 @@
 ﻿using System.IO;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace BoardEditorJeopardy;
 
 public partial class UserEditorWindow : Window
 {
-    private List<string> _users;
+    private List<TextBox> _usersTextBoxes = [];
+    private List<string> _users => _usersTextBoxes.Select(x => x.Text).ToList();
     
     
     public UserEditorWindow (List<string> users)
     {
         InitializeComponent();
 
-        _users = users;
-        Users.ItemsSource = _users;
+        _usersTextBoxes.AddRange(users.Select(x => new TextBox
+        {
+            Text = x,
+            MinWidth = 150
+        }));
+        _usersTextBoxes.ForEach(x => Users.Items.Add(x));
     }
     
+    
+    private void AddUser_Click (object sender, RoutedEventArgs e)
+    {
+        var newUser = new TextBox
+        {
+            Text = "New User",
+            MinWidth = 150
+        };
+        _usersTextBoxes.Add(newUser);
+        Users.Items.Add(newUser);
+    }
+    
+    private void RemoveUser_Click (object sender, RoutedEventArgs e)
+    {
+        if (Users.SelectedItem is not TextBox selectedUser)
+        {
+            return;
+        }
+        
+        _usersTextBoxes.Remove(selectedUser);
+        Users.Items.Remove(selectedUser);
+    }
     
     private void SaveButton_Click (object sender, RoutedEventArgs e)
     {

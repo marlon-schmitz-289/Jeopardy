c025b98445391fe627282fac1eeeff0924d74f70
Schmitz Marlon <marlonschmitz289@gmail.com>
2025-04-08 14:27:05 +0200

    Added UserSelection
    Still a bit buggy

--- PunktestandJeopardy/MainWindow.xaml.cs
+++ PunktestandJeopardy/MainWindow.xaml.cs
@@ -6,25 +6,43 @@ namespace PunktestandJeopardy;
 
 public partial class MainWindow : Window
 {
-    public MainWindow (IEnumerable<string> usernames)
+    public MainWindow(IEnumerable<string> usernames)
     {
         InitializeComponent();
         InitializeUsers(usernames);
     }
 
-    
-    private void InitializeUsers (IEnumerable<string> usernames)
+
+    private void InitializeUsers(IEnumerable<string> usernames)
     {
-        var users = usernames.ToArray();
-        PlayerOne.Content = users[0];
-        PlayerTwo.Content = users[1];
-        PlayerThree.Content = users[2];
+        // TODO: Open new window to select users to use
+        var users = UserSelection.Show(usernames.ToList());
+
+        if (users.Count < 2)
+        {
+            MessageBox.Show("Please select at least 2 users");
+            Application.Current.Shutdown();
+        }
+
+        if (users.Count > 2)
+        {
+            PlayerOneThreeUsers.Content = users[0];
+            PlayerTwoThreeUsers.Content = users[1];
+            PlayerThreeThreeUsers.Content = users[2];
+            return;
+        }
+
+        PlayerOneTwoUsers.Content = users[0];
+        PlayerTwoTwoUsers.Content = users[1];
     }
 
-    
-    private void Label_MouseLeftButtonDown (object sender, MouseButtonEventArgs e)
+
+    private void Label_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
     {
-        if (sender is not Label { Content: string s } l)
+        if (sender is not Label
+        {
+            Content: string s
+        } l)
         {
             return;
         }
@@ -38,10 +56,13 @@ public partial class MainWindow : Window
         l.Content = i.ToString();
     }
 
-    
-    private void Label_MouseRightButtonDown (object sender, MouseButtonEventArgs e)
+
+    private void Label_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
     {
-        if (sender is not Label { Content: string s } l)
+        if (sender is not Label
+        {
+            Content: string s
+        } l)
         {
             return;
         }
---
+++ PunktestandJeopardy/UserSelection.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace PunktestandJeopardy;
+
+public partial class UserSelection : Window
+{
+    private List<string> _selectedUsers = [];
+    
+    private UserSelection(List<string> usernames)
+    {
+        InitializeComponent();
+        InitializeUsers(usernames);
+    }
+
+    private void InitializeUsers(List<string> usernames)
+    {
+        Users.ItemsSource = usernames;
+    }
+
+    private void Confirm_Click(object sender, RoutedEventArgs e)
+    {
+        _selectedUsers = Users.SelectedItems.OfType<string>().ToList();
+    }
+    
+    
+    public static List<string> Show(List<string> usernames)
+    {
+        var window = new UserSelection(usernames);
+        window.ShowDialog();
+        return window._selectedUsers;
+    }
+
+    private void SelectedUsers_Changed(object sender, SelectionChangedEventArgs e)
+    {
+        if (e.AddedItems.Count > 0)
+        {
+            _selectedUsers.AddRange(e.AddedItems.OfType<string>().ToList());
+        }
+
+        if (e.RemovedItems.Count > 0)
+        {
+            _selectedUsers.RemoveAll(e.RemovedItems.OfType<string>().ToList().Contains);
+        }
+    }
+}

855b96168d41b20084202e617fe61929bef3d654
Marlon Schmitz <80980610+marlon-schmitz-289@users.noreply.github.com>
2025-04-12 18:41:40 +0200

    Fixed weird formatting

--- PunktestandJeopardy/Question.cs
+++ PunktestandJeopardy/Question.cs
@@ -9,14 +9,14 @@ public enum QuestionType
 
 public class Question
 {
-
-
     public Question (string question, string answer, QuestionType type = QuestionType.Text)
     {
         Questi = question;
         Answer = answer;
         QuType = type;
     }
+    
+    
     public string Questi { get; set; }
     public string Answer { get; set; }
     public QuestionType QuType { get; set; }
